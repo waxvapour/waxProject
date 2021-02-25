96d82bed9e81cf708ab1f0ffc4d18646d6b18878
waxvapour <waxvapour@gmail.com>
2021-02-25 12:51:27 +0530

    first test commit

---
+++ ALL OLD MeM/index.php
@@ -0,0 +1,10 @@
+<?php require_once('./core/includes/basicHead.php'); ?>
+
+<div class="w3-container w3-margin w3-padding">
+	<div onclick="explorer()" class="w3-col m1 w3-center w3-hover-text-brown w3-text-dark-grey" style="min-width:10%">
+		<i class="fa fa-folder-open fa-4x w3-padding" aria-hidden="true"></i>
+      <span class="Dicon">Files</span>
+	</div>
+</div>
+
+<?php require_once('./core/includes/footer.php'); ?>
