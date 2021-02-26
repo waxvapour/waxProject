09bf85890956857ef661b932c1c69ae960b0a3e5
waxvapour <waxvapour@gmail.com>
2021-02-26 15:04:01 +0530

    fooling around with material

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 
 <nav class="nav-extended indigo darken-1">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">[W]</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
         <li><a href="collapsible.html">JavaScript</a></li>
       </ul>
+    </div>
+    <div class="row">
+       <div class="col s12 6">
+          <img src="assets/logoWhite.png" width="30%" class="col s6 m2">
+      </div>
     </div>
     <div class="nav-content">
       <ul class="tabs tabs-transparent">
